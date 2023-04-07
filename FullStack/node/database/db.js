376facc240684b47ca5dcd40fe95475ae1be3673
Richard960227 <93538292+Richard960227@users.noreply.github.com>
2023-04-07 11:45:05 -0600

    Delete FullStack directory

--- FullStack/node/database/db.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const db = new Sequelize('database_app', 'root', '',{
-    host:'localhost',
-    password: '759213',
-    dialect: 'mysql'
-});
-
-export default db;
